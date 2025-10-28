ff29b834b0f15be15724f8152b233af4fe4afa8c
NitaiGupta <nitaigupta2003@gmail.com>
2025-10-28 18:57:07 +0530

    numpy in python

---
+++ Code/Numpy.py
@@ -0,0 +1,35 @@
+import numpy as np
+
+# a= np.array([1,2,3,4,5])
+# print(a)
+# print(type(a))
+# print(a.shape)
+
+# arr=np.array([[1,2,3],[4,5,6]] )
+# print(arr)
+# print(arr.shape)
+
+# a=np.zeros((3,3))
+# b=np.ones((3,3))
+# print(b)
+# print(a.shape)
+
+
+# '''Arrays of some constants'''
+# c=np.full((3,2),5)
+# print(c)
+
+
+# '''Identity Matrix'''
+
+# d=np.eye(4)
+# print(d)
+
+
+# '''Random Matrix'''
+# e=np.random.random((2,3))
+# print(e)
+
+
+# e=np.random.random((2,3))
+# print(e.dtype)
